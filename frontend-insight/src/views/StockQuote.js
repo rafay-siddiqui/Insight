e42643bcc059c0b04857da9a2c9a914f111fe6f3
RafaySiddiqui <rafays.siddiqui@gmail.com>
2021-10-29 15:25:50 -0400

    Stash changes

--- frontend-insight/src/views/StockQuote.js
+++ frontend-insight/src/views/StockQuote.js
@@ -8,18 +8,23 @@ export default function StockQuote(props) {
 
   const [stockData, setStockData] = useState([])
 
+  const generateGraph = (array) => {
+    array.map(point => console.log(point))
+  }
+
   const fetchData = (ticker) => {
     hyperlinkTicker(ticker)
       .then((res) => setStockData(res)
       )
+      .then(res => generateGraph([...stockData]))
   }
 
   return (
     <div>
       <h2>Stock Quote</h2>
       <Button login onClick={() => fetchData(props.ticker)}>API Call</Button>
-      <Button login onClick={ () => console.log(stockData[5]) }>Console Log</Button>
-      {stockData && <ExampleChart series={stockData}/>}
+      <Button login onClick={() => console.log(stockData[5])}>Console Log</Button>
+      {/* {stockData && <ExampleChart series={stockData}/>} */}
     </div>
   )
 }
